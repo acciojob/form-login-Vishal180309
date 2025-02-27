673c453d57ef86659de3e8fee4e0564e3c524cc5
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-27 08:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
+document.getElementById("myForm").addEventListener("submit", getFormValue);
 
+function getFormValue(e) {
+    e.preventDefault();
+    const fname = document.getElementById("fname").value.trim();
+    const lname = document.getElementById("lname").value.trim();
+    const fullName = `${fname} ${lname}`;
+    alert(fullName);
 }
